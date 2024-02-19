c59fced6da2bd4a4a059698561b8ff4593c0b365
Rachel Larson <126201983+RLarson92@users.noreply.github.com>
2024-02-19 14:52:42 -0600

    Update CoOccurance_Model.R

--- R/CoOccurance_Model.R
+++ R/CoOccurance_Model.R
@@ -1,5 +1,4 @@
-setwd("~/Documents/For School/Iowa/Dissertation Research/Multi-City Squirrels/R/MultiSpecies")
-# load functions used to clean data
+# load housekeeping & utility functions
 functions_to_load <- list.files(
   "./R/functions/",
   full.names = TRUE
@@ -9,10 +8,10 @@ for(fn in functions_to_load){
 }
 library(tidyr)
 
-# read in the data
-# allSquirrels <- read.csv("./data/allSquirrels.csv", stringsAsFactors = FALSE)
+# read in the detection/non-detection data for each species. I tend to use the same abbreviation for the species
+# names throughout: 'FS' = fox squirrel; 'GS' = gray squirrel; 'RS' = red squirrel
 FS_occu <- read.csv("./data/FS.csv", stringsAsFactors = FALSE)
-fox_dets <- FS_occu[,3:38]
+fox_dets <- FS_occu[,3:38] # pulling out the columns that have all the 1/0/NA's
 GS_occu <- read.csv("./data/GS.csv", stringsAsFactors = FALSE)
 gray_dets <- GS_occu[,3:38]
 RS_occu <- read.csv("./data/RS.csv", stringsAsFactors = FALSE)
@@ -24,28 +23,20 @@ GS_stack <- wide_to_stacked(gray_dets, 4, 2)
 RS_stack <- wide_to_stacked(red_dets, 4, 3)
 # bind them together in order by species
 y <- rbind(FS_stack, GS_stack, RS_stack)
-# stack the replicates (weeks) on top of each other and order the datasheet. Data should be in order by Species, then season, then city, then site
+# stack the replicates (weeks) on top of each other and order the datasheet. Data should be in order by species, then season, then city, then site
 y_long <- gather(y, key = "Week", value = "Y", week1:week4, factor_key = TRUE)
 y_long <- y_long[order(
   y_long[,"Species"], 
   y_long[,"Season"],
   y_long[,"City"],
   y_long[,"Site"]),]
+# converting some text-based columns to numeric values
 y_long$Week <- as.numeric(y_long$Week)
 y_long$Site <- as.numeric(as.factor(y_long$Site))
 y_long$City <- as.numeric(as.factor(y_long$City))
 
-# Code from Mason
-# "This is not a complete example, but this would be the idea. Get numeric IDs for each species, site, replicate, and year in a big data.frame as well as what value that element should take. Then use those to subset your array."
-# my_data <- data.frame(
-  # species = c(1,1,1,2,2,2,3,3,3,3),
-  # site = c(1,1,1,1,2,2,2,2,2,2),
-  # replicate = c(1,2,1,2,1,2,1,2,1,2),
-  # year = c(1,1,1,1,1,2,2,2,2,2),
-  # value = c(1,1,1,0,0,0,0,1,1,NA)
-# )
-
-# create a blank array that has the correct dimensions
+# create a blank array that has the correct dimensions, in this case 4 
+# (species by site by week by season)
 my_array <- array(
   NA,
   dim = c(
@@ -65,9 +56,11 @@ for(i in 1:nrow(y_long)){
   ] <- y_long$Y[i]
 }
 
-# pulling out detection covariates
+# reading in detection-level covariates (# active camera-days)
 obs_covs <- read.csv("./data/cooc_obsCovs.csv", stringsAsFactors = FALSE)
-FS_obs <- wide_to_stacked(obs_covs[,3:38], 4, 1) # to 38 for full dataset
+# this data.frame needs to be in stacked format and ordered, the same as
+# the detection histories, so let's do that
+FS_obs <- wide_to_stacked(obs_covs[,3:38], 4, 1) 
 GS_obs <- wide_to_stacked(obs_covs[,3:38], 4, 2)
 RS_obs <- wide_to_stacked(obs_covs[,3:38], 4, 3)
 obs <- rbind(FS_obs, GS_obs, RS_obs)
@@ -79,6 +72,8 @@ obs_long <- obs_long[order(
   obs_long[,"Site"]),]
 obs_long$Week <- as.numeric(obs_long$Week)
 obs_long$Site <- as.numeric(as.factor(obs_long$Site))
+
+# make the array and fill it in
 obsArray <- array(data = NA, dim = c(max(obs_long$Species),
                                      max(obs_long$Site),
                                      max(obs_long$Week),
@@ -94,8 +89,11 @@ for(i in 1:nrow(obs_long)){
   ] <- obs_long$J[i]
 }
 
+# we will handle occupancy covariates below, under the 'JAGS' heading, as they
+# vary from model to model
+
 # last but not least, making the array of possible occupancy states
-# (e.g., 1/1/1 = all species present; 0/1/0 = only gray squirrel present)
+# (e.g., 1/1/1 = all species present; 0/1/0 = only gray squirrel present, etc.)
 Xcat <- matrix(c(1, 1, 1,
                  1, 0, 0,
                  0, 1, 0,
@@ -107,78 +105,10 @@ Xcat <- matrix(c(1, 1, 1,
 
 library(runjags)
 ##### JAGS #####
-#### Fit the model (geographic model) ####
-# pulling out the continuous site covariates & scaling them
-site_covariates <- read.csv("./data/cooc_covs.csv", stringsAsFactors = FALSE)
-continuous_covariates <- site_covariates[,12]
-continuous_covariates <- scale(continuous_covariates)
-covs_for_model <- cbind.data.frame("imperv"=continuous_covariates[,1],
-                                   "city"=site_covariates[,2],
-                                   "site"=site_covariates[,1]
-)
-covs_for_model$city <- as.numeric(as.factor(covs_for_model$city))
-covs_for_model$site <- as.numeric(as.factor(covs_for_model$site))
-covMatrix <- as.matrix(covs_for_model)
-
-hasFox <- read.csv("./data/hasFox.csv", stringsAsFactors = FALSE)
-hasFox$City <- as.numeric(as.factor(hasFox$City))
-hasFox$Site <- as.numeric(as.factor(hasFox$Site))
-foxMatrix <- as.matrix(hasFox)
-hasGray <- read.csv("./data/hasGray.csv", stringsAsFactors = FALSE)
-hasGray$City <- as.numeric(as.factor(hasGray$City))
-hasGray$Site <- as.numeric(as.factor(hasGray$Site))
-grayMatrix <- as.matrix(hasGray)
-hasRed <- read.csv("./data/hasRed.csv", stringsAsFactors = FALSE)
-hasRed$City <- as.numeric(as.factor(hasRed$City))
-hasRed$Site <- as.numeric(as.factor(hasRed$Site))
-redMatrix <- as.matrix(hasRed)
-
-# Data list for model
-data_list <- list(
-  nspec = max(y_long$Species),
-  ncity = max(y_long$City),
-  nsite = max(y_long$Site),
-  imperv = covMatrix[,1],
-  nrep = max(y_long$Week),
-  trapDays = obsArray, #needs to be a 4-dimensional array
-  y = my_array, #needs to be a 4-dimensional array
-  nseason = max(y_long$Season),
-  Xcat = Xcat,
-  city_vec = as.numeric(factor(FS_occu$City)),
-  hasFox = foxMatrix[,3],
-  hasGray = grayMatrix[,3],
-  hasRed = redMatrix[,3]
-)
-
-# Fit the model
-my_start <- Sys.time()
-geo_mod <- runjags::run.jags(
-  model = "./JAGS/multiSpec_model_geoRange.R",
-  monitor = c("a0_mu", "a0_sd", "a1_mu", "a1_sd", "a0", "a1",
-              "b0_mu", "b0_sd", "b1_mu", "b1_sd", "b0", "b1",
-              "c0_mu", "c0_sd", "c1_mu", "c1_sd", "c0", "c1",
-              "d0_mu", "d0_sd", "d1_mu", "d1_sd", "d0", "d1",
-              "e0_mu", "e0_sd", "e1_mu", "e1_sd", "e0", "e1",
-              "g0_mu", "g0_sd", "g1_mu", "g1_sd", "g0", "g1",
-              "p0_mu", "p0_sd", "p1_mu", "p1_sd", "p0", "p1",
-              "phi_mu", "phi_sd", "phi", "x"),
-  data = data_list,
-  n.chains = 3,
-  inits = my_inits,
-  burnin = 50000,
-  sample = 20000,
-  adapt = 500,
-  modules = "glm",
-  thin = 2,
-  method = "parallel"
-)
-my_end <- Sys.time()
-print(geo_mod)
-saveRDS(my_mod, "./results/geographic_model.RDS")
-
 #### Fit the Model (PCA model) ####
+# read in site-level covariates
 site_covariates <- read.csv("./data/cooc_covs.csv", stringsAsFactors = FALSE)
-continuous_covariates <- site_covariates[,c(8,11,14)]
+continuous_covariates <- site_covariates[,c(8,11,14)] # pulling out just the covariates, sans the city/site names
 library(psych)
 PCA <- prcomp(continuous_covariates, center = TRUE, scale. = TRUE)
 summary(PCA)
